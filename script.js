261c4b74ef3ead9e5257c8dc0c50b9b6aba5de92
Auriol-Ivan <auriol.ivan@gmail.com>
2024-10-17 20:02:21 -0300

    carrusel corregido

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-let indice = 0;
-const imagenes = document.querySelectorAll('.imagenes img');
+//carrusel
+
+const imagenes = [
+    'img/hoja.jpg',
+    'img/mosaico.jpg',
+    'img/ojo.jpg'
+];
+
+let index = 0;
 
 function mostrarImagen() {
-    imagenes.forEach((img, i) => {
-        img.classList.remove('activo'); // Esconde todas las imágenes
-        if (i === indice) {
-            img.classList.add('activo'); // Muestra la imagen activa
-        }
-    });
+    const imgElement = document.getElementById('imagen');
+    imgElement.src = imagenes[index];
 }
 
-function cambiarImagen(direccion) {
-    indice += direccion;
-    if (indice < 0) {
-        indice = imagenes.length - 1; // Volver a la última imagen
-    } else if (indice >= imagenes.length) {
-        indice = 0; // Volver a la primera imagen
-    }
+function cambiarImagen(n) {
+    index = (index + n + imagenes.length) % imagenes.length;
     mostrarImagen();
 }
 
-// Cambia la imagen cada 3 segundos
-setInterval(() => cambiarImagen(1), 5000);
-
+mostrarImagen();
 
 // Validar formulario contacto
 document.getElementById("contactForm").addEventListener("submit", function(event) {
